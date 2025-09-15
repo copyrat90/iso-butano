6fcd6da1159f731b1770a5cc7f997315f7b0df31
copyrat90 <copyrat90@gmail.com>
2025-09-15 16:18:00 +0900

    Add `sram_rw`

---
+++ include/ibn_make_unsigned_allow_bool.h
@@ -0,0 +1,27 @@
+// SPDX-FileCopyrightText: Copyright 2021-2025 Guyeon Yu <copyrat90@gmail.com>
+// SPDX-License-Identifier: Zlib
+
+#pragma once
+
+#include <cstdint>
+#include <type_traits>
+
+namespace ibn
+{
+
+template <typename T>
+struct make_unsigned_allow_bool
+{
+    using type = std::make_unsigned_t<T>;
+};
+
+template <>
+struct make_unsigned_allow_bool<bool>
+{
+    using type = std::uint8_t;
+};
+
+template <typename T>
+using make_unsigned_allow_bool_t = typename make_unsigned_allow_bool<T>::type;
+
+} // namespace ibn
